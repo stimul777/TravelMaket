c3f36a1a5c62dd2d00e5b67139057c7c2989ec4a
Юрий <yydevlyashov@gmail.com>
2020-04-02 11:00:53 +0400

    settings OK

--- build/menu/menu.js
+++
@@ -1,9 +0,0 @@
-const menuItem = document.querySelectorAll('.menu_item');
-console.log(menuItem);
-
-for (let item of menuItem) {
-  item.addEventListener('click', () => {
-  event.target.style.backgroundColor = '#68de8a';
-  console.log(item); 
-  });
-}
